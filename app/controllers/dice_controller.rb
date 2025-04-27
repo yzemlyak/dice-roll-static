0210506e1e1b8ece6ed0a008e1a9966ee49488f3
yzemlyak <yzemlyak@gmail.com>
2025-04-27 00:04:50 +0000

    Basically final

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -5,4 +5,13 @@ class DiceController < ApplicationController
   def two_ten
     render ({ :template => "dice_templates/2d10"})
   end
+  def one_twenty
+    render ({ :template => "dice_templates/1d20"})
+  end
+  def five_four
+    render ({ :template => "dice_templates/5d4"})
+  end
+  def homepage
+    render ({ :template => "dice_templates/homepage"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
-  
+  get("/", { :controller => "dice", :action => "homepage"})
   get("/dice/2/6", { :controller => "dice", :action => "two_six" })
   get("/dice/2/10", { :controller => "dice", :action => "two_ten" })
+  get("/dice/1/20", { :controller => "dice", :action => "one_twenty" })
+  get("/dice/5/4", { :controller => "dice", :action => "five_four" })
 end
